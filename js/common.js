0e246e41ca8e840d0f3e7e7417704e50d8e6e7f6
hsei0510 <113411195+hsei0510@users.noreply.github.com>
2022-11-05 12:06:46 +0900

    최초 업로드

---
+++ js/common.js
@@ -0,0 +1,45 @@
+$(function(){
+
+  // console.log($(window).height());
+
+  $(window).scroll(function(){
+    //console.log($(this)); // scrollTop을 알아보기위해서 해본것!
+    // console.log($(this).scrollTop());
+    var winHeight = $(window).height();
+    var scTop = $(this).scrollTop();
+
+    var navBar = $('nav');
+    // navBar.addClass('fixed')
+    console.log(navBar);
+
+    // console.log(`window-height: ${winHeight}, scroll-top: ${scTop}`);
+    if(scTop > winHeight - 30){
+      // console.log('메뉴 고정');
+      navBar.addClass('fixed');
+    }else{
+      // console.log('메뉴 고정해제');
+      navBar.removeClass('fixed');
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
